ce615a1f94631b45639f30ba9d9c46ae5f9732ea
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2023-01-15 15:55:22 +0300

    install and configure dependencies
    libraries: react-router, firebase, zustand, eslint, prettier, mui
    also added aliases to vite config

---
+++ src/routes/root.jsx
@@ -0,0 +1,29 @@
+import { Outlet } from 'react-router-dom';
+import { useMemo } from 'react';
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const Root = () => {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Outlet />
+    </ThemeProvider>
+  );
+};
+
+export default Root;
