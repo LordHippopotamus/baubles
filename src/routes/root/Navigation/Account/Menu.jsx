8878428e7ff2ef5f9854e8f49a4df111776abefb
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2023-04-01 18:14:21 +0000

    feat: display spiner while sign out

---
+++ src/routes/root/Navigation/Account/Menu.jsx
@@ -0,0 +1,62 @@
+import { CircularProgress, List, ListItem, ListItemButton, Popover } from '@mui/material';
+import { useEffect } from 'react';
+import { Link, useFetcher } from 'react-router-dom';
+import { routes } from 'utils/routes';
+
+const Menu = ({ anchor, handleClose }) => {
+  const fetcher = useFetcher();
+  const loading = fetcher.state !== 'idle';
+
+  const handleSignOut = () => {
+    fetcher.submit({ action: 'signOut' }, { method: 'post', action: routes.home });
+  };
+
+  useEffect(() => {
+    fetcher.state === 'idle' && fetcher.data && handleClose();
+  }, [fetcher.state, fetcher.data]);
+
+  return (
+    <Popover
+      anchorEl={anchor}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      open={!!anchor}
+      onClose={handleClose}
+    >
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleClose} component={Link} to={routes.profile} disabled={loading}>
+            Profile
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton disabled={loading} onClick={handleSignOut}>
+            {loading ? (
+              <div
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                <CircularProgress color="inherit" size="1rem" />
+              </div>
+            ) : (
+              'Sign Out'
+            )}
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Popover>
+  );
+};
+
+export default Menu;
