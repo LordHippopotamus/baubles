15f9854148173208f47a2fec6e7d93bb825ab229
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2023-04-07 17:34:21 +0300

    fix: top offset in svg

--- src/utils/generateArea.js
+++ src/utils/generateArea.js
@@ -1,8 +1,8 @@
 export const generateArea = (columns, rows) => {
   const area = [];
 
-  for (let x = 1; x <= columns; x++) {
-    for (let y = 1; y <= rows; y++) {
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
       area.push({ x, y, color: null });
     }
   }
