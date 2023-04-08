040b34814c2c31f2414c73cd2add937cea837b10
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2023-04-08 07:45:16 +0300

    refactor: move translate and scale login to hook

--- src/routes/editor/Area/index.jsx
+++ src/routes/editor/Area/index.jsx
@@ -1,39 +1,17 @@
 import { Box } from '@mui/material';
-import { useState } from 'react';
 import { useSelectedTool } from 'hooks/editor';
+import { useTransform } from 'hooks/transform';
 import Canvas from './Canvas';
 
 const Area = () => {
   const tool = useSelectedTool();
 
-  const [scale, setScale] = useState(1);
-  const [translate, setTranslate] = useState({ x: 0, y: 0 });
-
-  const handleScale = event => {
-    if (tool === 'pan') {
-      const delta = event.wheelDelta ? event.wheelDelta : -event.deltaY;
-      const nextScale = delta > 0 ? scale * 1.2 : scale * 0.8;
-      setTranslate({
-        x: (nextScale / scale) * (translate.x - event.clientX) + event.clientX,
-        y: (nextScale / scale) * (translate.y - event.clientY) + event.clientY,
-      });
-      setScale(nextScale);
-    }
-  };
-
-  const handleTranslate = event => {
-    if (event.buttons === 1 && tool === 'pan') {
-      setTranslate({
-        x: translate.x + event.movementX,
-        y: translate.y + event.movementY,
-      });
-    }
-  };
+  const { scale, handleScale, translate, handleTranslate } = useTransform();
 
   return (
     <Box
-      onWheel={handleScale}
-      onMouseMove={handleTranslate}
+      onWheel={tool === 'pan' && handleScale}
+      onMouseMove={tool === 'pan' && handleTranslate}
       height="calc(100vh - 64px)"
       overflow="hidden"
     >
