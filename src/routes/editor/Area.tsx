f6798fc649adb758b841b0942835f087214a8b3a
Daniel <91888300+LordHippopotamus@users.noreply.github.com>
2023-05-20 21:00:55 +0300

    improve touch experience in editor

--- src/routes/editor/Area.tsx
+++ src/routes/editor/Area.tsx
@@ -1,12 +1,19 @@
 import { useSelectedTool, useSelectedColor, useDraw, useArea } from 'hooks/editor';
 import Preview from 'components/Preview';
-import { MouseEvent } from 'react';
+import { MouseEvent, TouchEvent, useEffect } from 'react';
 
 const Area = () => {
   const area = useArea();
   const tool = useSelectedTool();
   const draw = useDraw();
   const selectedColor = useSelectedColor();
+
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, []);
 
   const handleMove = (event: MouseEvent & { target: SVGPathElement }) => {
     if (event.buttons === 1 && event.target.tagName === 'path') {
@@ -22,11 +29,22 @@ const Area = () => {
     if (tool === 'eraser') draw(+x, +y, null);
   };
 
+  const handleTouch = (event: TouchEvent & { target: SVGPathElement }) => {
+    const target = document.elementFromPoint(
+      event.changedTouches[0].clientX,
+      event.changedTouches[0].clientY
+    );
+    if (!target) return;
+    const [x, y] = target.id.split(';');
+    if (tool === 'brush') draw(+x, +y, selectedColor);
+    if (tool === 'eraser') draw(+x, +y, null);
+  };
+
   return (
     <Preview
       area={area}
       wrapperProps={{ disabled: tool !== 'pan' }}
-      svgProps={{ onMouseMove: handleMove }}
+      svgProps={{ onMouseMove: handleMove, onTouchMove: handleTouch }}
       rectProps={{ onClick: handleClick }}
     />
   );
